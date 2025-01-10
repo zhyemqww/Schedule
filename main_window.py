4942340350f8e42d9b4a8a180f703be1345b378d
zhyemqww <zhyemqww@dicp.ac.cn>
2025-01-10 14:30:19 +0800

    change pushbutton color

--- main_window.py
+++ main_window.py
@@ -24,7 +24,7 @@ class MainWindow(BaseFramelessWindow):
         "Reset": "rgba(255, 255, 255, 0.7)",
         "This Week": "rgba(237, 125, 49, 0.7)",
         "Next Week": "rgba(255, 217, 102, 0.7)",
-        "Week After Next": "rgba(197, 190, 180, 0.7)",
+        "Week After Next": "rgba(197, 224, 180, 0.7)",
         "Missing": "rgba(207, 213, 234, 0.7)",
         "Skip": "rgba(68, 114, 196, 0.7)",
         "Finished": "rgba(65, 200, 40, 0.7)",
@@ -32,7 +32,7 @@ class MainWindow(BaseFramelessWindow):
         "Reset_highlight": "rgba(255, 255, 255, 1)",
         "This Week_highlight": "rgba(237, 125, 49, 1)",
         "Next Week_highlight": "rgba(255, 217, 102, 1)",
-        "Week After Next_highlight": "rgba(197, 190, 180, 1)",
+        "Week After Next_highlight": "rgba(197, 224, 180, 1)",
         "Missing_highlight": "rgba(207, 213, 234, 1)",
         "Skip_highlight": "rgba(68, 114, 196, 1)",
         "Finished_highlight": "rgba(65, 200, 40, 1)",
@@ -40,10 +40,10 @@ class MainWindow(BaseFramelessWindow):
         "Reset_downplay": "rgba(255, 255, 255, 0.2)",
         "This Week_downplay": "rgba(237, 125, 49, 0.2)",
         "Next Week_downplay": "rgba(255, 217, 102, 0.2)",
-        "Week After Next_downplay": "rgba(197, 190, 180, 0.2)",
+        "Week After Next_downplay": "rgba(197, 224, 180, 0.2)",
         "Missing_downplay": "rgba(207, 213, 234, 0.2)",
-        "Skip_downplay": "rgba(68, 114, 196, 0.2)",
-        "Finished_downplay": "rgba(65, 200, 40, 0.2)",
+        "Skip_downplay": "rgba(68, 114, 196, 0.5)",
+        "Finished_downplay": "rgba(65, 200, 40, 0.5)",
     }
 
 
